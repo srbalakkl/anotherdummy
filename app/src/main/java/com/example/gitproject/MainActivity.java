0c4dfc1421f3d04585c8121901703d713ab76a5e
SR.Bala <sr.bala.kkl@gmail.com>
2021-03-15 17:17:22 +0530

    initial commit with i

--- app/src/main/java/com/example/gitproject/MainActivity.java
+++ app/src/main/java/com/example/gitproject/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.gitproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int i=0;
+        Log.i("tg namej", "onCreate: the i output is"+i);
     }
 }
